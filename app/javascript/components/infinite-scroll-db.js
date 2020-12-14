4ca516f7c2be66fd0de4809ce760447a77163253
alexis <alexis59495@gmail.com>
2020-12-14 11:34:33 +0100

    Finalize infinite loop

--- app/javascript/components/infinite-scroll-db.js
+++ app/javascript/components/infinite-scroll-db.js
@@ -1,9 +1,9 @@
 const infiniteScroll = () => {
   if(document.querySelector('.container-database')){
     const THRESHOLD = 30;
-    const paginationElem = $('.container-pagination');
     const window_screen = $(window);
     const document_screen = $(document);
+    let paginationElem = $('.container-pagination');
     const paginationUrl = paginationElem.attr('data-pagination-endpoint');
     const pagesAmount = paginationElem.attr('data-pagination-pages');
     let currentPage = 1;
@@ -37,6 +37,17 @@ const infiniteScroll = () => {
   
     // Listen to scrolling
     window_screen.on('scroll', () => {
+      // Handle the search, in this case we reset the variable - CurrentPage - to 1
+      let paginationElem = $('.container-pagination');
+      if (paginationElem.attr('data-search') == "result") {
+        currentPage = 1;
+        paginationElem.attr('data-search', 'scroll');
+      };
+      if(currentPage >= pagesAmount) {
+        setTimeout(function(){
+          paginationElem.hide();
+        }, 1000);
+      };
       if(!isPaginating && currentPage < pagesAmount && window_screen.scrollTop() > document_screen.height() - window_screen.height() - THRESHOLD) {
         isPaginating = true;
         currentPage++;
