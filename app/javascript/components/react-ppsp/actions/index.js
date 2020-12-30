0cc2f26940e8c7215b430761423cc674ec3ded22
alexis <alexis59495@gmail.com>
2020-12-31 00:17:00 +0100

    Add search on first name and last name in react

--- app/javascript/components/react-ppsp/actions/index.js
+++ app/javascript/components/react-ppsp/actions/index.js
@@ -1,29 +1,28 @@
-export const FETCH_PPSPS = 'FETCH_PPSPS'
-export const SEARCH_QUERY = 'SEARCH_QUERY'
-export const LOAD_MORE = 'LOAD_MORE'
-
+export const FETCH_PPSPS = 'FETCH_PPSPS';
+export const SEARCH_QUERY = 'SEARCH_QUERY';
+export const LOAD_MORE = 'LOAD_MORE';
 
 export const fetchPpsps = (showUser) => {
-  const promise = fetch(`/api/v1/ppsps?show_user=${showUser}`)
-    .then(response => response.json())
+  const promise = fetch(
+    `/api/v1/ppsps?show_user=${showUser}`
+  ).then((response) => response.json());
   return {
     type: FETCH_PPSPS,
     payload: promise,
   };
-}
+};
 
-export const loadMore = (showUser, page) => {
-  const promise = fetch(`/api/v1/ppsps?show_user=${showUser}&page=${page}`)
-    .then(response => response.json())
+export const loadMore = (showUser, page, search) => {
+  const promise = fetch(
+    `/api/v1/ppsps?show_user=${showUser}&page=${page}&search=${search}`
+  ).then((response) => response.json());
   return {
     type: LOAD_MORE,
     payload: promise,
   };
-}
+};
 
-export const searchPpsp = (query) => {
-  return {
-    type: SEARCH_QUERY,
-    search: query
-  };
-}
+export const searchPpsp = (query) => ({
+  type: SEARCH_QUERY,
+  search: query,
+});
--- app/javascript/components/react-ppsp/reducers/ppsps_reducer.js
+++ app/javascript/components/react-ppsp/reducers/ppsps_reducer.js
@@ -1,45 +1,54 @@
-import { SEARCH_QUERY } from '../actions/index'
-import { FETCH_PPSPS } from '../actions/index'
-import { LOAD_MORE } from '../actions/index'
+import { SEARCH_QUERY, FETCH_PPSPS, LOAD_MORE } from '../actions/index';
 
-
-import { initialState } from '../index'
-
+import { initialState } from '../index';
 
 const reducerPpsps = (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_PPSPS: 
+    case FETCH_PPSPS:
       return {
         ...state,
         ppsps: action.payload,
         // Just select the 12 first elements
-        selectedPpsps: action.payload.slice(0,12),
+        selectedPpsps: action.payload.slice(0, 12),
         // Reinitialize the infinite scroll when use the search bar
         page: 2,
-      }
+      };
     case LOAD_MORE:
       return {
         ...state,
         // If the list is not empty before the first search action
         selectedPpsps: state.selectedPpsps.concat(action.payload),
-        page: state.page + 1
-      }
+        page: state.page + 1,
+      };
     case SEARCH_QUERY:
-      // Select the Ppsps which reference correpond to the value of search and limit the result to 12 max.
-      let count = 0
-      const selected = state.ppsps.filter((ppsp) => {
-        return ppsp.project_information.reference.toLowerCase().includes(action.search.toLowerCase()) && count++ < 12
-      })
+      let count = 0;
+      const selected = state.ppsps.filter(
+        (ppsp) =>
+          // Select the Ppsps which reference correspond to the value of search
+          (ppsp.project_information.reference
+            .toLowerCase()
+            .includes(action.search.toLowerCase()) ||
+            // Select the Ppsps which user first name correspond to the value of search
+            ppsp.user.first_name
+              .toLowerCase()
+              .includes(action.search.toLowerCase()) ||
+            // Select the Ppsps which user last name correspond to the value of search
+            ppsp.user.last_name
+              .toLowerCase()
+              .includes(action.search.toLowerCase())) &&
+          // Limit the result to 12 max
+          count++ < 12
+      );
       return {
         ...state,
         // Reinitialize the infinite scroll when use the search bar
         page: 2,
         search: action.search,
-        selectedPpsps: selected
-      }
+        selectedPpsps: selected,
+      };
     default:
       return state;
   }
-}
+};
 
-export default reducerPpsps ;
+export default reducerPpsps;
