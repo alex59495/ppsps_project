125445834a6a7a5f27b91fca1164d86087b4b384
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2021-01-20 13:23:58 +0100

    Test ok

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -5,6 +5,7 @@ RSpec.feature "Home View", type: :feature, js: true do
     visit(root_path)
     find('#btn-display-modal').click
     expect(page).to have_css('.show')
+    expect(page).not_to have_css('.swal2-backdrop-show')
   end
 
   scenario 'Send an email' do
@@ -14,6 +15,6 @@ RSpec.feature "Home View", type: :feature, js: true do
     fill_in('email', with: 'test@gmail.com')
     fill_in('description', with: 'My reasons')
     find('#btn-send-mail').click
-    expect(page).to have_css('.')
+    expect(page).to have_css('.swal2-backdrop-show')
   end
 end
