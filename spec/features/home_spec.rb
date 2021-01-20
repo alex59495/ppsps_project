d1f7390bf430b1c8a50d11e4a727f262de9ea5f2
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2021-01-20 13:18:54 +0100

    test view

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -6,4 +6,14 @@ RSpec.feature "Home View", type: :feature, js: true do
     find('#btn-display-modal').click
     expect(page).to have_css('.show')
   end
+
+  scenario 'Send an email' do
+    visit(root_path)
+    find('#btn-display-modal').click
+    fill_in('name', with: 'Lenoir MAXENCE')
+    fill_in('email', with: 'test@gmail.com')
+    fill_in('description', with: 'My reasons')
+    find('#btn-send-mail').click
+    expect(page).to have_css('.')
+  end
 end
