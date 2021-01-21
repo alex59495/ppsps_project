60ef334a66830f80280907c20ed94c3021b6f464
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2021-01-21 09:06:21 +0100

    Error message

--- app/javascript/components/sweetAlert.js
+++ app/javascript/components/sweetAlert.js
@@ -1,18 +1,21 @@
 import Swal from 'sweetalert2';
 
-const initSweetAlert = (selector, options = {}) => {
-  const modal = document.getElementById(selector);
-  if (modal) {
-    Swal.fire(options);
-  }
-};
-
 const sweetAlertMail = () => {
-  initSweetAlert('message-sent', {
-    icon: 'success',
-    title: 'Message envoyé',
-    text: 'Merci de votre sollicitation. Nous revenons vers vous très bientôt.',
-  });
+  if (document.getElementById('message-sent')) {
+    Swal.fire({
+      icon: 'success',
+      title: 'Message envoyé',
+      text:
+        'Merci de votre sollicitation./n Nous revenons vers vous très bientôt.',
+    });
+  } else if (document.getElementById('message-not-sent')) {
+    Swal.fire({
+      icon: 'error',
+      title: "Problème lors de l'envoi",
+      text:
+        "Une erreur est survenue empêchant l'envoi de l'email. Merci de réessayer plus tard/n Désolé pour le désagrément",
+    });
+  }
 };
 
 export default sweetAlertMail;
