e664378070cb3e082151ffe1702c2056d891ccf9
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2021-01-23 10:58:38 +0100

    Rectif passage à la ligne sur Swal

--- app/javascript/components/sweetAlert.js
+++ app/javascript/components/sweetAlert.js
@@ -5,15 +5,15 @@ const sweetAlertMail = () => {
     Swal.fire({
       icon: 'success',
       title: 'Message envoyé',
-      text:
-        'Merci de votre sollicitation.\n Nous revenons vers vous très bientôt.',
+      html:
+        'Merci de votre sollicitaiton.</br>Nous revenous vers vous très prochainement.',
     });
   } else if (document.getElementById('message-not-sent')) {
     Swal.fire({
       icon: 'error',
       title: "Problème lors de l'envoi",
-      text:
-        "Une erreur est survenue empêchant l'envoi de l'email. Merci de réessayer plus tard\n Désolé pour le désagrément",
+      html:
+        "Une erreur est survenue empêchant l'envoi de l'email. Merci de réessayer plus tard.</br> Désolé pour le désagrément",
     });
   }
 };
